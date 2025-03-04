1edcd323b37d0dafa750a74534dd012552884434
Siddhanta Acharya <siddhu12980@gmail.com>
2025-03-04 15:25:08 +0530

    :tada:init with poetry

---
+++ assistant/assistant/main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+print(driver.title)
+
+driver.quit()
